1dc7503e7e8551daa5347d22cd331bcca1e9c4e2
Samuel <Samuel.tukua@gmail.com>
2018-10-26 10:36:31 -0400

    Update OpprebornTeleOp.java

--- OpprebornTeleOp.java
+++ OpprebornTeleOp.java
@@ -30,24 +30,29 @@ public class OpprebornTeleOp extends LinearOpMode {
         oppreborn.init(hardwareMap);
         waitForStart();
         while (opModeIsActive()){
-            updateForwardDrive();
+            updateDrive();
             updateHanger();
             updateStrafe();
+
+            idle();
         }
     }
-private synchronized void updateForwardDrive(){
-        oppreborn.leftDrive.setPower(gamepad1.left_stick_y);
-        oppreborn.rightDrive.setPower(gamepad1.right_stick_x);
+private synchronized void updateDrive(){
+        oppreborn.leftDrive.setPower(gamepad1.left_stick_y*.7);
+        oppreborn.rightDrive.setPower(gamepad1.right_stick_y*.7);
 }
 private synchronized void updateHanger(){
-        if (gamepad1.left_trigger>0 && gamepad1.right_trigger>0){
-            oppreborn.liftnLower.setPower(DROP_SPEED);
+        if (gamepad1.left_trigger>0 || gamepad1.right_trigger>0){
+            oppreborn.liftnLower.setPower((-((gamepad1.left_trigger/2) + (gamepad1.right_trigger/2)))*.7);
         }
         else if (gamepad1.left_bumper && gamepad1.right_bumper){
-            
+            oppreborn.liftnLower.setPower(DROP_SPEED);
+        }
+        else {
+            oppreborn.liftnLower.setPower(0);
         }
 }
 private synchronized void updateStrafe(){
-
+        oppreborn.midDrive.setPower(((gamepad1.right_stick_x/2)+(gamepad1.left_stick_x/2))*.7);
 }
     }
